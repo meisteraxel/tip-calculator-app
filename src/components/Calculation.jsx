46cca31937668faa5dc67942bdccfa75798748f5
Axel <meister.axel@gmail.com>
2024-04-03 15:53:43 +0200

    Finished Calculation Component Styling

--- src/components/Calculation.jsx
+++ src/components/Calculation.jsx
@@ -3,22 +3,22 @@ import "../index.css";
 
 function Calculation() {
   return (
-    <div className="bg-very-dark-cyan p-6 rounded-md w-1/2 flex flex-col">
-      <div className="flex justify-between items-center">
+    <div className="bg-very-dark-cyan p-8 rounded-2xl w-1/2 flex flex-col">
+      <div className="flex justify-between items-center mt-2">
         <div>
           <p className="text-white font-semibold text-m">Tip Amount</p>
           <p className="text-grayish-cyan text-sm">/ person</p>
         </div>
-        <p className="text-strong-cyan text-4xl font-bold">$4.27</p>
+        <p className="text-strong-cyan text-5xl font-bold">$4.27</p>
       </div>
-      <div className="flex justify-between items-center mt-5">
+      <div className="flex justify-between items-center mt-12">
         <div>
           <p className="text-white font-semibold text-m">Total</p>
           <p className="text-grayish-cyan text-sm">/ person</p>
         </div>
-        <p className="text-strong-cyan text-4xl font-bold">$32.27</p>
+        <p className="text-strong-cyan text-5xl font-bold">$32.27</p>
       </div>
-      <button className="bg-strong-cyan rounded-md py-1 w-full mt-auto font-semibold text-very-dark-cyan">
+      <button className="bg-strong-cyan rounded-md py-3 w-full mt-auto text-xl font-bold text-very-dark-cyan">
         RESET
       </button>
     </div>
